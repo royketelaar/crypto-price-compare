daaa835c3fdb32a1cc4aab8742f67a7519ecbcff
royketelaar <royketelaar@gmail.com>
2020-02-06 15:39:51 +0100

    added interface

--- src/app/interfaces/data.ts
+++ src/app/interfaces/data.ts
@@ -1,3 +1,25 @@
+export interface Price {
+    bitvavo:        Bitcoinmeester;
+    bitcoinmeester: Bitcoinmeester;
+    litebit:        Bitcoinmeester;
+    bitonic:        Bitcoinmeester;
+    bitmymoney:     Bitcoinmeester;
+    satos:          Bitcoinmeester;
+    bitrush:        Bitcoinmeester;
+}
+
+export interface Bitcoinmeester {
+    name:      string;
+    link_buy:  string;
+    link_sell: string;
+    data:      Data;
+}
+
 export interface Data {
-  
+    success:   boolean;
+    value:     string;
+    btc:       string;
+    min_buy:   string;
+    min_sell:  string;
+    available: string;
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http' 
+import { HttpClient } from '@angular/common/http'
+import { Price } from '../interfaces/data'
 
 @Injectable()
 export class DataService {
