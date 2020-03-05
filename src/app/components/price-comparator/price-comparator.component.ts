7ff6e3a3d886a80e28a68d267178c1b5fad9d523
royketelaar <royketelaar@gmail.com>
2020-03-05 12:52:33 +0100

    now shows DOGE instead of BTC

--- src/app/components/price-comparator/price-comparator.component.ts
+++ src/app/components/price-comparator/price-comparator.component.ts
@@ -28,38 +28,60 @@ export class PriceComparatorComponent implements OnInit {
     this.buy_data = []
     this.sell_data = []
 
-    // Bitonic
-    this.http.get('https://bitonic.nl/api/buy').subscribe((data) =>  {
-      this.buy_data.push({ broker: 'Bitonic', price: parseFloat(data.eur).toFixed(2) })
-      this.sortData()
+    // // Bitonic
+    // this.http.get('https://bitonic.nl/api/buy').subscribe((data) =>  {
+    //   this.buy_data.push({ broker: 'Bitonic', price: parseFloat(data.eur).toFixed(2) })
+    //   this.sortData()
 
-    })
+    // })
 
-    this.http.get('https://bitonic.nl/api/sell').subscribe((data) => {
-      this.sell_data.push({ broker: 'Bitonic', price: parseFloat(data.eur).toFixed(2) })
-      this.sortData()
-    })
+    // this.http.get('https://bitonic.nl/api/sell').subscribe((data) => {
+    //   this.sell_data.push({ broker: 'Bitonic', price: parseFloat(data.eur).toFixed(2) })
+    //   this.sortData()
+    // })
 
-    // Litebit
-    this.http.get('https://api.litebit.eu/market/btc').subscribe((data) => {        
-        this.buy_data.push({ broker: 'Litebit', price: parseFloat(data.result.buy).toFixed(2) })
-        this.sell_data.push({ broker: 'Litebit', price: parseFloat(data.result.sell).toFixed(2) })       
-        this.sortData() 
-      }
-    )
-
-
-
-
-    // this.http.get('https://api.bitvavo.com/v2/ticker/price').subscribe((data) => {
-    //     const correct_object = data.filter(item => item.market == 'BTC-EUR')
-    //     this.bitvavo_buy_price = correct_object[0].price
+    // // Litebit
+    // this.http.get('https://api.litebit.eu/market/btc').subscribe((data) => {        
+    //     this.buy_data.push({ broker: 'Litebit', price: parseFloat(data.result.buy).toFixed(2) })
+    //     this.sell_data.push({ broker: 'Litebit', price: parseFloat(data.result.sell).toFixed(2) })       
+    //     this.sortData() 
     //   }
     // )
+    // Sistemkoin
+    this.http.get('https://api.sistemkoin.com/ticker').subscribe((data) => {
+      this.buy_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.askPrice).toFixed(8) })
+      this.sell_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.bidPrice).toFixed(8) })
+      this.sortData() 
+    })
+
+    // Alterdice
+    this.http.get('https://api.alterdice.com/v1/public/ticker?pair=DOGEUSD').subscribe((data) => {
+      this.buy_data.push({ broker: 'Alterdice', price: parseFloat(data.data.high).toFixed(8) })
+      this.sell_data.push({ broker: 'Alterdice', price: parseFloat(data.data.low).toFixed(8) })
+      this.sortData() 
+    })
+
+    // Exmo
+    this.http.get('https://api.exmo.com/v1/ticker/').subscribe((data) => {
+      this.buy_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.buy_price).toFixed(8) })
+      this.sell_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.sell_price).toFixed(8) })
+      this.sortData() 
+    })
+
+    // SouthXChange
+    this.http.get('https://www.southxchange.com/api/prices').subscribe((data) => {
+      const correct_object = data.filter(item => item.Market == 'DOGE/USD')
+      const buy_price = correct_object[0].Ask
+      const sell_price = correct_object[0].Bid
+
+      this.buy_data.push({ broker: 'SouthXChange', price: parseFloat(buy_price).toFixed(8) })
+      this.sell_data.push({ broker: 'SouthXChange', price: parseFloat(sell_price).toFixed(8) })
+      this.sortData() 
+    })
     
   }
   sortData() {
     this.buy_data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-    this.sell_data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    this.sell_data.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
   }
 }
