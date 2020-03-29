fc4184888cd778534163f2f590de1132ff10485d
royketelaar <royketelaar@gmail.com>
2020-03-29 22:37:41 +0200

    work in progress

--- src/app/components/price-comparator/price-comparator.component.ts
+++ src/app/components/price-comparator/price-comparator.component.ts
@@ -26,41 +26,45 @@ export class PriceComparatorComponent implements OnInit {
     this.buy_data = []
     this.sell_data = []
 
-    // Sistemkoin
-    this.http.get('https://api.sistemkoin.com/ticker').subscribe((data) => {
-      this.buy_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.askPrice).toFixed(8) })
-      this.sell_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.bidPrice).toFixed(8) })
-      this.total_apis++
-      this.sortData() 
-    })
+    // // Sistemkoin
+    // this.http.get('https://api.sistemkoin.com/ticker').subscribe((data) => {
+    //   this.buy_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.askPrice).toFixed(8) })
+    //   this.sell_data.push({ broker: 'Sistemkoin', price: parseFloat(data.USD.DOGE.bidPrice).toFixed(8) })
+    //   this.total_apis++
+    //   this.sortData() 
+    // })
 
-    // Alterdice
-    this.http.get('https://api.alterdice.com/v1/public/ticker?pair=DOGEUSD').subscribe((data) => {
-      this.buy_data.push({ broker: 'Alterdice', price: parseFloat(data.data.high).toFixed(8) })
-      this.sell_data.push({ broker: 'Alterdice', price: parseFloat(data.data.low).toFixed(8) })
-      this.total_apis++
-      this.sortData() 
+    // // Alterdice
+    // this.http.get('https://api.alterdice.com/v1/public/ticker?pair=DOGEUSD').subscribe((data) => {
+    //   this.buy_data.push({ broker: 'Alterdice', price: parseFloat(data.data.high).toFixed(8) })
+    //   this.sell_data.push({ broker: 'Alterdice', price: parseFloat(data.data.low).toFixed(8) })
+    //   this.total_apis++
+    //   this.sortData() 
 
-    })
+    // })
 
-    // Exmo
-    this.http.get('https://api.exmo.com/v1/ticker/').subscribe((data) => {
-      this.buy_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.buy_price).toFixed(8) })
-      this.sell_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.sell_price).toFixed(8) })
-      this.total_apis++
-      this.sortData() 
-    })
+    // // Exmo
+    // this.http.get('https://api.exmo.com/v1/ticker/').subscribe((data) => {
+    //   this.buy_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.buy_price).toFixed(8) })
+    //   this.sell_data.push({ broker: 'Exmo', price: parseFloat(data.DOGE_USD.sell_price).toFixed(8) })
+    //   this.total_apis++
+    //   this.sortData() 
+    // })
 
-    // SouthXChange
-    this.http.get('https://www.southxchange.com/api/prices').subscribe((data) => {
-      const correct_object = data.filter(item => item.Market == 'DOGE/USD')
-      const buy_price = correct_object[0].Ask
-      const sell_price = correct_object[0].Bid
+    // // SouthXChange
+    // this.http.get('https://www.southxchange.com/api/prices').subscribe((data) => {
+    //   const correct_object = data.filter(item => item.Market == 'DOGE/USD')
+    //   const buy_price = correct_object[0].Ask
+    //   const sell_price = correct_object[0].Bid
 
-      this.buy_data.push({ broker: 'SouthXChange', price: parseFloat(buy_price).toFixed(8) })
-      this.sell_data.push({ broker: 'SouthXChange', price: parseFloat(sell_price).toFixed(8) })
-      this.total_apis++
-      this.sortData() 
+    //   this.buy_data.push({ broker: 'SouthXChange', price: parseFloat(buy_price).toFixed(8) })
+    //   this.sell_data.push({ broker: 'SouthXChange', price: parseFloat(sell_price).toFixed(8) })
+    //   this.total_apis++
+    //   this.sortData() 
+    // })
+
+    this.http.get('https://api.coinranking.com/v1/public/markets').subscribe((data) => {
+      console.log(data)
     })
     
   }
